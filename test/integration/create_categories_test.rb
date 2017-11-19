f20aa7631c974698a67a6540ba1637110419d584
Daniel Kunicki <dankunicki@gmail.com>
2017-11-19 15:52:59 +0000

    Fix Create Categories Test after adding admin requirement

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -3,9 +3,11 @@ require 'test_helper'
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
   
   def setup
+    @admin_user = User.create(username: "John", email: "jon@example.com", password: "password", admin: true)
   end
   
   test "get new category form and create category" do
+    sign_in_as(@admin_user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -16,6 +18,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
   
   test "invalid category submission results in failure" do 
+    sign_in_as(@admin_user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 0 do
